d4e6397d24db033e807eb7f8ff16298bfedbf46e
Izzati Liyana <izztyana06@gmail.com>
2026-01-16 12:54:07 +0800

    Create Enumerator Example 2.cpp

---
+++ SECJ1013 Programming Technique I/C8 Structured Data/Enumerator Example 2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Enum declaration
+enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+};
+
+int main() {
+    OrderStatus status = PENDING;
+
+    if (status == PENDING) {
+        cout << "Order is pending." << endl;
+    }
+
+    status = PROCESSING;
+    cout << "Order is being processed." << endl;
+
+    status = SHIPPED;
+    cout << "Order has been shipped." << endl;
+
+    switch (status) {
+    case PENDING:
+        cout << "Pending order" << endl;
+        break;
+    case PROCESSING:
+        cout << "Processing order" << endl;
+        break;
+    case SHIPPED:
+        cout << "Order shipped" << endl;
+        break;
+    case DELIVERED:
+        cout << "Order delivered" << endl;
+        break;
+    case CANCELLED:
+        cout << "Order cancelled" << endl;
+        break;
+    }
+
+    int statuss = 3;   // What does 3 mean?
+
+    return 0;
+}
