f23b3d5b3bd0749101cced6932ff61bcc9a8add5
Izzati Liyana <izztyana06@gmail.com>
2026-01-20 00:57:04 +0800

    Create Dynamic memory allocation.cpp

---
+++ SECJ1013 Programming Technique I/C7 Pointers/Dynamic memory allocation.cpp
@@ -0,0 +1,36 @@
+/* This program totals and average the sales figures for any number of days.
+The figures are stored in dynamically allocated array */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    double *sales, total = 0.0, average;
+    int numDays, count;
+
+    cout << "Hoe many days of sales figures do you wish to process? ";
+    cin >> numDays;
+
+    // Dynamically allocate an array large enough to hold the days or else it would terminate
+    sales = new double[numDays];
+
+    cout << "Enter the sales figures for each day below." << endl;
+    for (count = 0; count < numDays; count++) {
+        cout << "Day " << (count + 1) << ": ";
+        cin >> sales[count];
+
+        total += sales[count];
+    }
+
+    average = total / numDays;
+
+    cout << fixed << showpoint << setprecision(2);
+    cout << "\nTotal Sales: $" << total << endl;
+    cout << "Average Sales: $" << average << endl;
+
+    delete [] sales; // Only for array, for others: delete sales; (no [])
+    sales = 0; // Makes sales point to null
+
+    return 0;
+}
