3c41d11c2baedd87bc6369f740cd508aeac7f976
Izzati Liyana <izztyana06@gmail.com>
2026-01-19 14:19:12 +0800

    Update Manipulator: Setprecision.cpp

--- SECJ1013 Programming Technique I/C6 Input & Output/Manipulator: Setprecision.cpp
+++ SECJ1013 Programming Technique I/C6 Input & Output/Manipulator: Setprecision.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main() {
     double x = 156.74, y = 235.765, z = 9525.9874;
-    cout << setprecision(5) << x << endl;
-    cout << setprecision(3) << x << endl;
-    cout << setprecision(2) << x << endl;
-    cout << setprecision(1) << x << endl;
+    cout << setprecision(5) << x << endl; // 156.74
+    cout << setprecision(3) << x << endl; // 157
+    cout << setprecision(2) << x << endl; // 1.6e+02
+    cout << setprecision(1) << x << endl; // 2e+02
     cout << fixed << setprecision(2);
-    cout << x << endl << y << endl << z << endl;
+    cout << x << endl << y << endl << z << endl; // z = 156.74
 
     return 0;
 }
