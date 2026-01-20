77d4bcea350bda30bfab50c7a1f7cedeba4ba33d
Izzati Liyana <izztyana06@gmail.com>
2026-01-20 20:38:13 +0800

    Create Structure return from a function.cpp

---
+++ SECJ1013 Programming Technique I/C8 Structured Data/Structure return from a function.cpp
@@ -0,0 +1,40 @@
+// This program uses a function to return a structure
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+const double PI = 3.14159;
+
+struct Circle
+{
+    double radius;
+    double diameter;
+    double area;
+};
+
+Circle getInfo();
+
+int main () {
+    Circle c;
+    c = getInfo();
+    c.area = PI * pow(c.radius, 2.0);
+
+    cout << "The radius and area of the circle are: " << endl;
+    cout << fixed << setprecision(2);
+    cout << "Radius: " << c.radius << endl;
+    cout << "Area: " << c.area << endl;
+
+    return 0;
+}
+
+Circle getInfo () {
+    Circle tempCircle; // Define local variable
+
+    cout << "Enter the diameter of a circle: ";
+    cin >> tempCircle.diameter;
+    tempCircle.radius = tempCircle.diameter / 2.0;
+
+    return tempCircle;
+}
