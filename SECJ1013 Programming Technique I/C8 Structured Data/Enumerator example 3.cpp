31086c24667b46292453c7666d6e5b9f47fa2b2d
Izzati Liyana <izztyana06@gmail.com>
2026-01-20 21:44:59 +0800

    Create Enumerator example 3.cpp

---
+++ SECJ1013 Programming Technique I/C8 Structured Data/Enumerator example 3.cpp
@@ -0,0 +1,28 @@
+// This program demonstrates another use of enumarated data type
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+enum Day {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY};
+
+int main () {
+    const int NUM_DAYS = 5;
+    double sales[NUM_DAYS];
+    double total = 0.0;
+    Day workDay; // Loop counter
+
+    for (workDay = MONDAY; workDay <= FRIDAY; workDay = static_cast<Day>(workDay + 1)) {
+        cout << "Enter the sales for day " << workDay << ": ";
+        cin >> sales[workDay];
+    }
+
+    for (workDay = MONDAY; workDay <= FRIDAY; workDay = static_cast<Day>(workDay + 1)) {
+        total += sales[workDay];
+    }
+
+    cout << "The total sales are $" << setprecision(2) << fixed << total << endl;
+    
+    return 0;
+
+}
